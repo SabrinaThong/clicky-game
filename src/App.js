c4d4039ae687b1cc0f92d782b4cbbe76f16d59dc
Sabrina Thong <sabrinaanhthong@gmail.com>
2019-06-20 17:35:31 -0500

    adding changes to images and html

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Navbar from "./Components/Navbar";
 
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p class="headContent">Clicky Game</p>  
-      </header>
+        <header>
+          <Navbar />
+        </header>
         <div class="jumbotron"><h1>Clicky Game</h1>
           <p>Click on an image to earn points, but don't click on an image more than once!</p>
         </div>
-        
 
-       
     </div>
+
   );
 }
 
