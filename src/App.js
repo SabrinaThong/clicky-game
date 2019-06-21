0c416fdf9f9dc1c6c499c84de08d7d6dba91bf2e
Sabrina Thong <sabrinaanhthong@gmail.com>
2019-06-20 23:40:43 -0500

    working onclick images and counter

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 import React from 'react';
 import './App.css';
-import Navbar from "./Components/Navbar";
-
+// import Navbar from "./Components/Navbar";
+import Images from "./Components/Images";
+import Counter from "./Components/Counter";
 
 function App() {
   return (
     <div className="App">
         <header>
-          <Navbar />
+          <p class="headContent">Clicky Game</p>
         </header>
         <div class="jumbotron"><h1>Clicky Game</h1>
           <p>Click on an image to earn points, but don't click on an image more than once!</p>
         </div>
+      
+      <Counter />
 
     </div>
 
   );
 }
+
 
 export default App;
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,17 +1,17 @@
-import React from "react";
+// import React from "react";
 
-function Navbar() {
-    return (
-    <Navbar>
-        <Navbar.Brand href="#home">Clicky Game</Navbar.Brand>
-        <Navbar.Toggle />
-        <Navbar.Collapse className="justify-content-end">
-        <Navbar.Text>
-            <p>Score:  | Top Score: </p>
-        </Navbar.Text>
-        </Navbar.Collapse>
-    </Navbar>
-    );
-};
+// function Navbar() {
+//     return (
+//     <Navbar>
+//         <Navbar.Brand href="#home">Clicky Game</Navbar.Brand>
+//         <Navbar.Toggle />
+//         <Navbar.Collapse className="justify-content-end">
+//         <Navbar.Text>
+//             Score:  | Top Score: 
+//         </Navbar.Text>
+//         </Navbar.Collapse>
+//     </Navbar>
+//     );
+// };
 
-export default Navbar;
+// export default Navbar;
